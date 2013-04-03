c0c105e0b737748329ea59dd726544901d65c17f
Allen Maxwell <aamax@xmission.com>
2013-04-02 19:26:38 -0600

    added user edit and create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,19 +7,49 @@ class UsersController < ApplicationController
   end
 
   def show
-      @user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
   
   def update
     authorize! :update, @user, :message => 'Not authorized as an administrator.'
+
     @user = User.find(params[:id])
-    if @user.update_attributes(params[:user], :as => :admin)
+    if (params[:user][:password] == "")
+      params[:user].delete("password")
+      params[:user].delete("password_confirmation")
+    end
+
+    if @user.update_attributes(params[:user])
+      # set to admin if flag set
+      if params["Administrator"].nil?
+        @user.remove_role :admin
+      else
+        @user.add_role :admin
+      end
+
       redirect_to users_path, :notice => "User updated."
     else
-      redirect_to users_path, :alert => "Unable to update user."
+      redirect_to users_path, :alert => "Unable to update user."  + @user.errors.full_messages.to_sentence
     end
   end
-    
+
+  def new
+    @newuser = User.new
+  end
+
+  def create
+    @newuser = User.new(params[:user])
+    if @newuser.save
+      redirect_to users_path
+    else
+      render :new, :alert => "Error creating user: " + @newuser.errors.full_messages.to_sentence
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def destroy
     authorize! :destroy, @user, :message => 'Not authorized as an administrator.'
     user = User.find(params[:id])
